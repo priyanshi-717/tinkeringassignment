8d9f0d15fe59e95591a6fe038cf7ff75bdbf295d
priyanshi-717 <prinshupatel001@gmail.com>
2026-04-16 04:12:58 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.start_time = 0
+        self.end_time = 0
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+
+    def get_time(self):
+        return self.end_time - self.start_time
